4eee5e0365ae7763d5a366e687aced5f00eb028e
Risman Hidayatulloh <risman100@gmail.com>
2023-10-04 13:30:38 +0700

    Add connect with postgresql

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/User';
 
 export const AppDataSource = new DataSource({
-    
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'admin123',
+  database: 'postgres',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
